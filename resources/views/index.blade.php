8375805bcbcdfe40899b8fa0b7f2d2217fd74af3
antoninodangelo11 <reapercsp@gmail.com>
2023-06-27 18:44:30 +0200

    add style

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -8,18 +8,35 @@
     </head>
 
     <body>
-        <ul>
-            @foreach ($trains as $train)
-                <li>{{ $train->company }}</li>
-                <li>{{ $train->departure_station }}</li>
-                <li>{{ $train->arrival_station }}</li>
-                <li>{{ $train->departure_date }}</li>
-                <li>{{ $train->arrival_date }}</li>
-                <li>{{ $train->train_code }}</li>
-                <li>{{ $train->number_of_carriages }}</li>
-                <li>{{ $train->in_time }}</li>
-                <li>{{ $train->deleted }}</li>
-            @endforeach
-        </ul>
+        <table class="table table-dark">
+            <thead>
+                <tr>
+                    <th class="text-danger">Company</th>
+                    <th class="text-danger">Departure Station</th>
+                    <th class="text-danger">Arrival Station</th>
+                    <th class="text-danger">Departure Date</th>
+                    <th class="text-danger">Arrival Date</th>
+                    <th class="text-danger">Train Code</th>
+                    <th class="text-danger">Number of Carriages</th>
+                    <th class="text-danger">In Time</th>
+                    <th class="text-danger">Deleted</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach($trains as $train)
+                    <tr>
+                        <td>{{ $train->company }}</td>
+                        <td>{{ $train->departure_station }}</td>
+                        <td>{{ $train->arrival_station }}</td>
+                        <td>{{ $train->departure_date }}</td>
+                        <td>{{ $train->arrival_date }}</td>
+                        <td>{{ $train->train_code }}</td>
+                        <td>{{ $train->number_of_carriages }}</td>
+                        <td>{{ $train->in_time }}</td>
+                        <td>{{ $train->deleted }}</td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
     </body>
 </html>
